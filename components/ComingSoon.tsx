5b1b14d3f862f4bc146d184e52780dbaf3ee4aa0
Daniel <dbjo@equinor.com>
2023-03-17 18:57:45 +0100

    feat: Add styling to landing page

--- components/ComingSoon.tsx
+++ components/ComingSoon.tsx
@@ -3,7 +3,7 @@ import styles from "../styles/comingSoon.module.css";
 export const ComingSoon = () => {
     return (
         <div className={styles.comingSoon}>
-            <h1>Coming soon!</h1>
+            <h1 className={styles.h1Header}>Coming soon!</h1>
         </div>
     )
 }
