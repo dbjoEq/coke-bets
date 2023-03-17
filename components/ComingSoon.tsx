d050f35a2fdada9665a14170cee2debb701cfeb7
Daniel <dbjo@equinor.com>
2023-03-17 18:51:17 +0100

    feat: Add temporary landing page

---
+++ components/ComingSoon.tsx
@@ -0,0 +1,9 @@
+import styles from "../styles/comingSoon.module.css";
+
+export const ComingSoon = () => {
+    return (
+        <div className={styles.comingSoon}>
+            <h1>Coming soon!</h1>
+        </div>
+    )
+}
