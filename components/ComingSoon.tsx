13cc5abba8b8aedfa22f04bb84194983203f39d9
Daniel <dbjo@equinor.com>
2023-03-18 09:50:39 +0100

    fix: Export default

--- components/ComingSoon.tsx
+++ components/ComingSoon.tsx
@@ -1,9 +1,11 @@
 import styles from "@/styles/comingSoon.module.css";
 
-export const ComingSoon = () => {
+const ComingSoon = () => {
     return (
         <div className={styles.comingSoon}>
             <h1 className={styles.h1Header}>Coming soon!</h1>
         </div>
     )
 }
+
+export default ComingSoon;
--- pages/index.tsx
+++ pages/index.tsx
@@ -1,7 +1,7 @@
 import Head from 'next/head'
 import { Inter } from 'next/font/google'
 import styles from '@/styles/Home.module.css'
-import { ComingSoon } from '@/components/ComingSoon'
+import ComingSoon from '@/components/ComingSoon'
 
 const inter = Inter({ subsets: ['latin'] })
 
